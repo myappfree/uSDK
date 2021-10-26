17248758f8c8885bf44ad5581216b806b7437998
Riccardo <riccardo@myappfree.com>
2021-10-26 12:24:09 +0200

    1.0.2

---
+++ Sources/uSDK.xcframework/ios-arm64_armv7/uSDK.framework/Headers/uSDK.h
@@ -0,0 +1,16 @@
+//
+//  uSDK.h
+//  uSDK
+//
+//  Created by Riccardo Fuzzi on 06/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for uSDK.
+FOUNDATION_EXPORT double uSDKVersionNumber;
+
+//! Project version string for uSDK.
+FOUNDATION_EXPORT const unsigned char uSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <uSDK/PublicHeader.h>
